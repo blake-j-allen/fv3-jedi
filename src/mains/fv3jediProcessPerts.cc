12670559dc8962d5f70099b049c04df4a0dd90d0
Yongming Wang <39815418+wangym1111@users.noreply.github.com>
2025-01-03 22:33:44 -0600

    Add fv3jediProcessPerts application (#1245)
    * Add fv3jediProcessPerts application
    
    * Add a ctest for processperts
    
    * Add ctest reference and update processperts_gfs.yaml according to the latest repository change
    
    * Update test/CMakeLists.txt for linking the reference file
    
    * Update src/mains/fv3jediProcessPerts.cc to follow alphabetical build order
    
    ---------
    
    Co-authored-by: Nate Crossette <ncrossette@ucar.edu>

---
+++ src/mains/fv3jediProcessPerts.cc
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright 2017-2020 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "fv3jedi/Utilities/Traits.h"
+#include "oops/runs/Run.h"
+#include "saber/oops/instantiateCovarFactory.h"
+#include "saber/oops/ProcessPerts.h"
+
+int main(int argc,  char ** argv) {
+  oops::Run run(argc, argv);
+  saber::instantiateCovarFactory<fv3jedi::Traits>();
+  saber::ProcessPerts<fv3jedi::Traits> processperts;
+  return run.execute(processperts);
+}
